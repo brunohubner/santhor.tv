f5460d352a288769c6dbbf5460716eeb8b801308
Bruno Hubner <brunohubnerdev@gmail.com>
2025-08-13 21:25:45 -0300

    feat: Add github.com/joho/godotenv v1.5.1 dependency

--- main.go
+++ main.go
@@ -5,24 +5,24 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 	"santhor.tv/internal/handler"
 	"santhor.tv/internal/youtube"
 )
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
 func main() {
-	port := getEnv("SERVER_PORT", "8080")
-	apiKey := getEnv("YOUTUBE_API_KEY", "")
-	channelID := getEnv("YOUTUBE_CHANNEL_ID", "")
+	var err error
 
-	if apiKey == "" || channelID == "" {
-		log.Fatal("As variáveis de ambiente YOUTUBE_API_KEY e YOUTUBE_CHANNEL_ID são obrigatórias.")
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("SERVER_PORT")
+	apiKey := os.Getenv("YOUTUBE_API_KEY")
+	channelID := os.Getenv("YOUTUBE_CHANNEL_ID")
+
+	if apiKey == "" || channelID == "" || port == "" {
+		log.Fatal("As variáveis de ambiente 'YOUTUBE_API_KEY', 'YOUTUBE_CHANNEL_ID' e 'SERVER_PORT' são obrigatórias.")
 	}
 
 	ytClient := youtube.NewClient(apiKey)
@@ -36,7 +36,7 @@ func main() {
 		Handler: mux,
 	}
 
-	log.Printf("Servidor iniciado na porta %s. Pronto para redirecionar santhor.tv!", port)
+	log.Printf("Servidor iniciado em http://localhost:%s \nPronto para redirecionar https://santhor.tv", port)
 
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("Falha ao iniciar o servidor: %v", err)
